aae9a6f8574525734396bdddcc26fb7efcdf4cf1
Maxime Kjaer <maxime.kjaer@gmail.com>
2014-07-17 14:55:23 +0200

    Update easy.py
    Added fix for leading zeros in binary conversion

--- Challenge-171/Easy/easy.py
+++ Challenge-171/Easy/easy.py
@@ -1,4 +1,4 @@
-hexvalue = 'FF 81 BD A5 A5 BD 81 FF'.split(' ')
-binary = [bin(int(line, 16))[2:] for line in hexvalue] #Convert it to a list of binary lines
-image = [pixel.replace('1', '* ').replace('0', '  ') for pixel in binary] #Convert it to a list of lines
-print '\n'.join(image) #Print it line by line
+hexvalue = '93 93 93 F3 F3 93 93 93'.split(' ')
+binary = [bin(int(line, 16))[2:].zfill(8) for line in hexvalue] #Convert it to a list of binary lines
+image = [pixel.replace('1', '*').replace('0', ' ') for pixel in binary] #Convert it to a list of lines
+print '\n'.join(image)
